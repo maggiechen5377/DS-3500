3e926479a67091a758f59c7265cadfa0814a01be
maggiechen5377 <maggiechen5377@gmail.com>
2026-02-04 14:37:39 -0500

    file changes

--- code/11_wrappers.py
+++ code/11_wrappers.py
@@ -14,22 +14,62 @@ def _code_mapping(df, src, targ):
     """Map labels in src and targ columns to integers.
     Return the df and labels
     Talk about internal/helper functions."""
+    # collect all the src and targ strings
+    all_labels = pd.concat([df[src], df[targ]]).unique()
+
+
+    # assign each one a number starting from 0
+
+    # create a mapping (dict)
+    labels = {}
+    for i in range(len(all_labels)):
+        labels[all_labels[i]] = i
+
+    # update src adn targ columns in the df
+    df[src] = df[src].map(lambda val: labels[val])
+    df[targ] = df[targ].map(lambda val: labels[val])
+    return df, labels
 
 
 def make_sankey(df, src, targ, vals, **kwargs):
     """Generate a sankey diagram from dataframe.
-    Talk about keyword args and positional args"""
+    Talk about keyword args and positional args
+    line_width: modify of line for links
+    """
+    df, mapping = _code_mapping(df,src,targ)
+
+    line_width = kwargs.get('line_width', None)
+
+    link = {'source': df[src], 'target': df[targ], 'values': df[vals],
+            'line': {"width": line_width}}
+    node = {'label': list(mapping.keys())}
+    fig = go.Figure(go.sankey(link=link, node=node))
+    fig.show()
 
 
 def demo_wrapper_basic():
     """Using the wrapper with simple bio data: organ → gene"""
     bio = pd.read_csv('data/bio.csv')
     make_sankey(bio, 'disease', 'gene', 'pubs')
+    fig.show()
+
+    bio2 = pd.read_csv('data/bio2.csv')
+    make_sankey(bio2, 'stage', 'organ', 'counts',
+                line_width=1, orientation='vertical')
+    fig.show()
 
 
 def demo_multi_layer_stacking():
     """Multi-layer Sankey via dataframe stacking: stage → organ → gene.
     """
+    bio = pd.read_csv('data/bio.csv')
+    bio.columns = ['source', 'target', 'value']
+    bio2 = pd.read_csv('data/bio2.csv')
+    bio2.columns = ['source', 'target', 'value']
+    combined = pd.concat([bio, bio2])
+    fig = make_sankey(combined, 'source', 'target', 'value')
+    fig.show()
+
 
 def practice_creating_sankey():
     '''Practice your own sankey diagram. Use the college dataset to study
@@ -41,7 +81,7 @@ def practice_creating_sankey():
 
 
 def main():
-    # demo_wrapper_basic()
+    demo_wrapper_basic()
     # demo_multi_layer_stacking()
     pass
 
